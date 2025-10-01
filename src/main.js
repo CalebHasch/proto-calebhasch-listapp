e2b912c1b5167ab31665b602cfff228874b15b77
Caleb <chasch96@gmail.com>
2025-10-01 18:54:31 -0400

    initiate auth

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "https://isqxxuuspycxnycyagnj.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzcXh4dXVzcHljeG55Y3lhZ25qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNjEyMzcsImV4cCI6MjA3NDczNzIzN30.XdyW6kt0t3FRm1sFlnVkBBHUN7C5863aA5O1gdsI4Vk"
-);
+import { initAuth } from "./stores/auth";
 
 const app = createApp(App);
 app.use(router);
